2076f07ed8c7200a4195fc4c207ea4b576274044
Irv <irvingarielm@gmail.com>
2024-10-29 00:33:18 -0700

    Correct variable names for deployment

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -2,16 +2,16 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
-if (!process.env.FIREBASE_PRIVATE_KEY || 
-    !process.env.FIREBASE_CLIENT_EMAIL || 
-    !process.env.FIREBASE_PROJECT_ID) {
-  throw new Error('Firebase Admin environment variables are not properly configured');
-}
+// if (!process.env.FIREBASE_PRIVATE_KEY || 
+//     !process.env.FIREBASE_CLIENT_EMAIL || 
+//     !process.env.FIREBASE_PROJECT_ID) {
+//   throw new Error('Firebase Admin environment variables are not properly configured');
+// }
 
 const firebaseAdminConfig = {
   credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID || "",
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
     privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
 })
 };
