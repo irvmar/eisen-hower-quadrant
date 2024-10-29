7c835bf529ab7ba2c9a804a01e8563400adcac07
Irv <irvingarielm@gmail.com>
2024-10-29 00:19:27 -0700

    Correct variable names for deployment

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -16,7 +16,7 @@ if (!apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       // The private key comes as a string with "\n" characters
       // We need to replace them with actual newlines
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
     }),
   });
 }
